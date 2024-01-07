5e89248f91f299541e83d50438aac14dc420b36d
Sandro <mazzarosandro@gmail.com>
2024-01-07 23:24:25 +0100

    Improve perror & add clang-format

--- src/log.c
+++ src/log.c
@@ -1,25 +1,8 @@
 /*
- * Copyleft (f) 2024 Sandroid75
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- *
- */
+ Copyleft 1991-2024 by Sandroid75
+
+ Plese read log.h
+*/
 
 #include <stdlib.h>
 #include <string.h>
@@ -30,121 +13,137 @@
 
 /* Struct declaration */
 static struct {
-  char *filelog;
-  FILE *fp;
-  int level;
-  int filelevel;
+	char *filelog;
+	FILE *fp;
+	int level;
+	int filelevel;
 } L = { NULL, NULL, LOG_NONE, LOG_NONE };
 
 /* Level string array */
 static const char *level_names[] = {
-  "NONE", "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+	"NONE",
+	"TRACE",
+	"DEBUG",
+	"INFO",
+	"WARN",
+	"ERROR",
+	"PERR",
+	"FATAL"
 };
 
 /* Escape color sequences for ANSI screen */
 #ifdef LOG_USE_COLOR
 static const char *level_colors[] = {
-  "\x1b[30m", "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+	"\x1b[30m",
+	"\x1b[94m",
+	"\x1b[36m",
+	"\x1b[32m",
+	"\x1b[33m",
+	"\x1b[31m",
+	"\x1b[34m",
+	"\x1b[35m"
 };
 #endif
 
 /* Function to set the log file of NULL to set a standard stream */
-void log_set_filelog(char *filelog) {
-  L.filelog = filelog;
+void log_set_filelog(char *filelog)
+{
+	L.filelog = filelog;
 }
 
 /* Function to set standard stream (filelog must be NULL), can be stdout or stderr */
-void log_set_fp(FILE *fp) {
-  L.fp = fp;
+void log_set_fp(FILE *fp)
+{
+	L.fp = fp;
 }
 
-/* Set the log level, can be LOG_NONE, LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL */
-void log_set_level(int level) {
-  L.level = level;
+/* Set the log level, can be LOG_NONE, LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_PERROR, LOG_FATAL */
+void log_set_level(int level)
+{
+	L.level = level;
 }
 
-/* Set the log file level, can be LOG_NONE, LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL */
-void log_set_file_level(int level) {
-  L.filelevel = level;
+/* Set the log file level, can be LOG_NONE, LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_PERROR, LOG_FATAL */
+void log_set_file_level(int level)
+{
+	L.filelevel = level;
 }
 
 /* Main log function */
-void log_log(int level, const int do_perror, const char *file, const char *function, int line, const char *fmt, ...)
+void log_log(int level, const char *file, const char *function, int line, const char *fmt, ...)
 {
-  int errnum = errno;
+	char err_msg[1024];
 
-  /* Get current time */
-  time_t t = time(NULL);
-  struct tm *lt = localtime(&t);
+	/* Check if perror must be print */
+	if (level == LOG_PERROR) {
+		strerror_r(errno, err_msg, sizeof(err_msg));
+	}
 
-  /* Log to stderr */
-  if (L.level != LOG_NONE && level >= L.level) {
-    va_list args;
-    char buf[16];
-    
-    /* Build the time stamp string */
-    buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
+	/* Get current time */
+	time_t t = time(NULL);
+	struct tm *lt = localtime(&t);
+
+	/* Log to stderr */
+	if (L.level != LOG_NONE && level >= L.level) {
+		va_list args;
+		char buf[16];
+
+		/* Build the time stamp string */
+		buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 
 #ifdef LOG_USE_COLOR
-    fprintf(stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", buf, level_colors[level], level_names[level], file, line);
+		fprintf(stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", buf, level_colors[level], level_names[level], file, line);
 #else
-    fprintf(stderr, "%s %-5s %s@%d: ", buf, level_names[level], file, line);
+		fprintf(stderr, "%s %-5s %s@%d: ", buf, level_names[level], file, line);
 #endif
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
+		va_start(args, fmt);
+		vfprintf(stderr, fmt, args);
+		va_end(args);
 
-    /* Check if perror must be print */
-    if (do_perror && level >= LOG_ERROR) {
-      char buff[1024];
-      
-      strerror_r(errnum, buff, sizeof(buff));
-      fprintf(stderr, ": %s", buff);
-    }
-    fprintf(stderr, "\n");
-    fflush(stderr);
-  }
+		if (level == LOG_PERROR) {
+			fprintf(stderr, ": %s", err_msg);
+		}
+		fprintf(stderr, "\n");
+		fflush(stderr);
+	}
 
-  /* Log to file */
-  if (L.filelevel != LOG_NONE && level >= L.filelevel) {
-    /* Open the file pointer or set the stream */
-    if (L.filelog) {                    // check if filelog is set
-        L.fp = fopen(L.filelog, "a+");
-        if(L.fp == NULL) {              // if fopen() fail
-            fprintf(stderr, "Unable to open '%s' the log will print on stderr\n", L.filelog);
-            L.fp = stderr;
-        }
-    } else if (L.fp && L.fp != stdout && L.fp != stderr) { // if file pointer is not standard output
-        L.fp = stderr;                                    // than set it to stderr
-    } else if (L.fp == NULL) {
-        return;
-    }
-    
-    va_list args;
-    char buf[32];
+	/* Log to file */
+	if (L.filelevel != LOG_NONE && level >= L.filelevel) {
+		/* Open the file pointer or set the stream */
+		if (L.filelog) { // check if filelog is set
+			L.fp = fopen(L.filelog, "a+");
+			if (L.fp == NULL) { // if fopen() fail
+				fprintf(stderr, "Unable to open '%s' the log will print on stderr\n", L.filelog);
+				L.fp = stderr;
+			}
+		} else if (L.fp && L.fp != stdout && L.fp != stderr) { // if file pointer is not standard output
+			L.fp = stderr; // than set it to stderr
+		} else if (L.fp == NULL) {
+			return;
+		}
 
-    /* Build the time stamp string */
-    buf[strftime(buf, sizeof(buf), "%Y%m%d %H:%M:%S", lt)] = '\0';
-    
-    fprintf(L.fp, "%s %-5s %s:%s(%d): ", buf, level_names[level], file, function, line);
-    va_start(args, fmt);
-    vfprintf(L.fp, fmt, args);
-    va_end(args);
-    
-    /* Check if perror must be print */
-    if (do_perror && level >= LOG_ERROR) {
-      char buff[1024];
-      
-      strerror_r(errnum, buff, sizeof(buff));
-      fprintf(L.fp, ": %s", buff);
-    }
-    fprintf(L.fp, "\n");
-    fflush(L.fp);
-  /* close the file */
-    if (L.fp != stdout && L.fp != stderr) {
-        fclose(L.fp);
-    }
-  }
+		va_list args;
+		char buf[32];
 
-  return;
+		/* Build the time stamp string */
+		buf[strftime(buf, sizeof(buf), "%Y%m%d %H:%M:%S", lt)] = '\0';
+
+		fprintf(L.fp, "%s %-5s %s:%s(%d): ", buf, level_names[level], file, function, line);
+		va_start(args, fmt);
+		vfprintf(L.fp, fmt, args);
+		va_end(args);
+
+		/* Check if perror must be print */
+		if (level == LOG_PERROR) {
+			fprintf(L.fp, ": %s", err_msg);
+		}
+		fprintf(L.fp, "\n");
+		fflush(L.fp);
+		/* close the file */
+		if (L.fp != stdout && L.fp != stderr) {
+			fclose(L.fp);
+		}
+	}
+
+	return;
 }
--- src/log.h
+++ src/log.h
@@ -1,10 +1,30 @@
 /*
- * Copyleft (f) 2024 Sandroid75
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MIT license. See `log.c` for details.
- *
- */
+ Copyleft 1991-2024 by Sandroid75
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met.
+ Redistributions of source code must retain the above copyleft notice,
+ this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyleft notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ Neither the name of Sandroid75 nor the names of its contributors may be used
+ to endorse or promote products derived from this software without specific 
+ prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYLEFT HOLDERS AND CONTRIBUTORS "AS IS" 
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ PURPOSE ARE DISCLAIMED. 
+ IN NO EVENT SHALL THE COPYLEFT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
 
 #ifndef LOG_H
 #define LOG_H
@@ -15,26 +35,32 @@ extern "C" {
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdbool.h>
 
 #define LOG_VERSION "0.3.0"
 
 /* log level */
-enum { LOG_NONE, LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+enum { LOG_NONE,
+	   LOG_TRACE,
+	   LOG_DEBUG,
+	   LOG_INFO,
+	   LOG_WARN,
+	   LOG_ERROR,
+	   LOG_PERROR,
+	   LOG_FATAL };
 
 /* Macro definitions */
 #if defined(NODEBUG) || defined(RELEASE)
-#define log_trace(...) 
-#define log_debug(...) 
+#define log_trace(...)
+#define log_debug(...)
 #else
-#define log_trace(...) log_log(LOG_TRACE, 0, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log(LOG_DEBUG, 0, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_trace(...) log_log(LOG_TRACE, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOG_DEBUG, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #endif
-#define log_info(...)  log_log(LOG_INFO,  0, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_log(LOG_WARN,  0, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log(LOG_ERROR, 0, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define log_perror(...) log_log(LOG_ERROR, 1, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) log_log(LOG_FATAL, 0, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_info(...)   log_log(LOG_INFO, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_warn(...)   log_log(LOG_WARN, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_error(...)  log_log(LOG_ERROR, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_perror(...) log_log(LOG_PERROR, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define log_fatal(...)  log_log(LOG_FATAL, __FILE__, __func__, __LINE__, __VA_ARGS__)
 /* Macro mnemonic definitions */
 #define LOGT log_trace
 #define LOGD log_debug
@@ -49,7 +75,7 @@ void log_set_filelog(char *filelog);
 void log_set_fp(FILE *fp);
 void log_set_level(int level);
 void log_set_file_level(int level);
-void log_log(int level, const int do_perror, const char *file, const char *function, int line, const char *fmt, ...);
+void log_log(int level, const char *file, const char *function, int line, const char *fmt, ...);
 
 #ifdef __cplusplus
 }
